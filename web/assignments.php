ac60d7dc992413dbd72cd20769e403c9927f5979
9snick4 <nic17014@byui.edu>
2019-01-18 16:38:28 +0100

    moved assignments.php

---
+++ web/assignments.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=12.0, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CS313 Assignments</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/one-page-wonder.css" rel="stylesheet">
+
+    <link href="css/stefano.css" rel="stylesheet">
+
+    <!--favicon-->
+    <link rel="shortcut icon" type="image/x-icon" href="img/icon.png"/>
+  </head>
+
+  <body>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/week01/includes/navigation.php';?>
+
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/week01/includes/header.php';?>
+
+    <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Coming soon!</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+    <?php require $_SERVER['DOCUMENT_ROOT']."/week01//includes/footer.php";?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
