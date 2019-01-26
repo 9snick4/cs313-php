bb5fef4f6a8a98f577114d73deb8fb92eb0143c1
Stefano Nicotra <9stefanonicotra4@gmail.com>
2019-01-27 00:37:47 +0100

    Add files via upload

---
+++ web/week3/templates/addToCart.php
@@ -0,0 +1,22 @@
+<?php
+	$redirectURL = "../profile.php";
+	/* 
+		Anytime we come here is trough the cart
+		$_GET[item] is the item selected to purchase
+		we want to increment the cookie val to allow the cart purchase
+		check if available and stuff
+	*/
+	
+	$curr_cookie=$_COOKIE["cart_cookie_".$_GET['item']];
+
+	echo $curr_cookie;
+	if(isset($curr_cookie)) {
+		setcookie("cart_cookie_".$_GET['item'], $curr_cookie+1, time() + (86400), "/"); 
+	}
+	else{
+		setcookie("cart_cookie_".$_GET['item'], 1, time() + (86400), "/"); 
+	}
+	
+	header('Location: '.$redirectURL);
+
+?>
