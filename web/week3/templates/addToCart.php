31bfbd742bdcddcbabd59f7fd69a8182c47c3865
Stefano Nicotra <snick94@byui.edu>
2019-01-27 02:26:09 +0100

    cart into user page

--- web/week3/templates/addToCart.php
+++ web/week3/templates/addToCart.php
@@ -5,6 +5,8 @@
 		$_GET[item] is the item selected to purchase
 		we want to increment the cookie val to allow the cart purchase
 		check if available and stuff
+
+	
 	*/
 	
 	$curr_cookie=$_COOKIE["cart_cookie_".$_GET['item']];
@@ -15,10 +17,13 @@
 	else{
 		setcookie("cart_cookie_".$_GET['item'], 1, time() + (86400), "/"); 
 	}
-	if(!isset($_COOKIE["cart_total"])) {
+	if(isset($_COOKIE["cart_total"])) {
+		setcookie("cart_total", .$_GET['cart_total']+1, time() + (86400), "/"); 
+	}
+	else{
 		setcookie("cart_total", 0, time() + (86400), "/"); 
 	}
-	setcookie("cart_total", .$_GET['cart_total']+1, time() + (86400), "/"); 
+	
 	header('Location: '.$redirectURL);
 
 ?>
