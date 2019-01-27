1c4cf061fd60d2aa6ce3d31cbfb146e9c761532d
Stefano Nicotra <snick94@byui.edu>
2019-01-27 02:28:28 +0100

    cart into user page

--- web/week3/templates/addToCart.php
+++ web/week3/templates/addToCart.php
@@ -17,12 +17,14 @@
 	else{
 		setcookie("cart_cookie_".$_GET['item'], 1, time() + (86400), "/"); 
 	}
+	/*
 	if(isset($_COOKIE["cart_total"])) {
 		setcookie("cart_total", .$_GET['cart_total']+1, time() + (86400), "/"); 
 	}
 	else{
 		setcookie("cart_total", 0, time() + (86400), "/"); 
 	}
+	*/
 	
 	header('Location: '.$redirectURL);
 
