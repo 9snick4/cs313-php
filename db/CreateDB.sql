4e046063461dd57418d155cba1830ed80827f16e
9snick4 <nic17014@byui.edu>
2019-02-01 12:28:19 +0100

    added db files

---
+++ db/CreateDB.sql
@@ -0,0 +1,21 @@
+CREATE TABLE City (
+    CityID SERIAL PRIMARY KEY NOT NULL,
+    CityName VARCHAR(58) NOT NULL,
+    Latitude FLOAT NOT NULL,
+    Longitude FLOAT NOT NULL,
+    Elevation FLOAT NOT NULL,
+    CityPopulation INT
+);
+
+CREATE TABLE Map (
+    MapID SERIAL PRIMARY KEY NOT NULL,
+    CityID INT REFERENCES City(CityID) NOT NULL
+);
+
+CREATE TABLE Game (
+    GameID SERIAL PRIMARY KEY NOT NULL,
+    MapID INT REFERENCES Map(MapID) NOT NULL,
+    GameDate DATE NOT NULL,
+    GameTime TIME NOT NULL,
+    Points INT NOT NULL
+);
