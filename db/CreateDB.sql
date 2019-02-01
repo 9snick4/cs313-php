8025059ceb99af3eecf3df85efb8d8a3aa827f57
9snick4 <nic17014@byui.edu>
2019-02-01 12:53:34 +0100

    improved db sql

--- db/CreateDB.sql
+++ db/CreateDB.sql
@@ -1,15 +1,43 @@
+CREATE TABLE Gamer (
+    GamerID SERIAL PRIMARY KEY NOT NULL,
+    GamerName VARCHAR(20) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
+
+
 CREATE TABLE City (
     CityID SERIAL PRIMARY KEY NOT NULL,
     CityName VARCHAR(58) NOT NULL,
     Latitude FLOAT NOT NULL,
     Longitude FLOAT NOT NULL,
     Elevation FLOAT NOT NULL,
-    CityPopulation INT
+    CityPopulation INT,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
 );
 
 CREATE TABLE Map (
     MapID SERIAL PRIMARY KEY NOT NULL,
-    CityID INT REFERENCES City(CityID) NOT NULL
+    MapName VARCHAR(58) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
+);
+
+CREATE TABLE MapCity (
+    MapCityID SERIAL PRIMARY KEY NOT NULL,
+    CityID INT REFERENCES City(CityID) NOT NULL,
+    MapID INT REFERENCES Map(MapID) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
 );
 
 CREATE TABLE Game (
@@ -17,5 +45,13 @@ CREATE TABLE Game (
     MapID INT REFERENCES Map(MapID) NOT NULL,
     GameDate DATE NOT NULL,
     GameTime TIME NOT NULL,
-    Points INT NOT NULL
+    Points INT NOT NULL,
+    GamerID INT REFERENCES Gamer(GamerID) NOT NULL,
+    CreatedOn TIMESTAMP NOT NULL,
+    ModifiedOn TIMESTAMP NOT NULL,
+    CreatedBy INT REFERENCES Gamer(GamerID) NOT NULL,
+    ModifiedBy INT REFERENCES Gamer(GamerID) NOT NULL
 );
+
+
+
